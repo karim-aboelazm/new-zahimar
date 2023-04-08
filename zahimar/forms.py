8a9550370cf5aa9d81fbfc6ab6117b9b8fe0c071
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2023-04-08 21:04:39 +0200

    Delete forms.py

--- zahimar/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ZahimarImagePrediction
-
-class ZahimarImagePredictionForm(forms.ModelForm):
-    class Meta:
-        model = 'ZahimarImagePrediction'
-        fields = ['image']
